fdf1c5ed5b3ce9315c1092102c0e447204ddb5f2
mathews-hos <mathewshosoares@gmail.com>
2023-05-17 13:52:27 -0300

    inicio da outra aula

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-function helloworld() {
-    alert("Hello World")
+function soma(n1, n2) {
+    return n1 + n2
 }
-helloworld()
+
+function subtracao(n1, n2) {
+    return n1 - n2
+}
+
+function multiplicacao(n1, n2) {
+    return n1 * n2
+}
+
+function divisao(n1, n2) {
+    return n1 / n2
+}
+
+
+function mostrarnome() {
+    let nome = prompt("Qual o seu nome?")
+    alert("olá, " + nome)
+}
+
+function arearetangulo(base, altura) {
+    return base * altura
+}
+
+function areacirculo(raio) {
+    return 3.1 * (raio * raio)  //pi x raio^2
+}
